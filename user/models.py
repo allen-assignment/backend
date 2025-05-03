1d7a76eaff60c410ca159d7ce70efacc220303ef
zeyu <wangzeyu654@gmail.com>
2025-05-03 13:45:42 +0800

    update login and register

--- user/models.py
+++ user/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+
 
 # Create your models here.
 class User(models.Model):
@@ -9,6 +11,17 @@ class User(models.Model):
     birth_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.password.startswith('pbkdf2_'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    class Meta:
+        db_table = 'user'
 
     def __str__(self):
         return self.username
--- user/views.py
+++ user/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+from .models import User
+from django.http import JsonResponse
+from django.contrib.auth.hashers import check_password, make_password
+import json
 # Create your views here.
+@csrf_exempt
+def user_login(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            username = data.get('username')
+            password = data.get('password')
+
+            if not username or not password:
+                return JsonResponse({'error': 'password and username must not be null'}, status=400)
+
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return JsonResponse({'error': 'User not existed'}, status=404)
+
+            if check_password(password, user.password):
+                return JsonResponse({'message': 'Login success', 'username': user.username})
+            else:
+                return JsonResponse({'error': 'Incorrect password'}, status=401)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+
+    return JsonResponse({'error': 'method error'}, status=405)
+
+@csrf_exempt
+def user_register(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            username = data.get('username')
+            password = data.get('password')
+            email = data.get('email')
+            birth_date = data.get('birth_date')
+            usertype = data.get('usertype')
+
+            if not all([username, password]):
+                return JsonResponse({'error': 'username and password must not be null'}, status=400)
+
+            if User.objects.filter(username=username).exists():
+                return JsonResponse({'error': 'user existed'}, status=400)
+
+            hashed_password = make_password(password)
+
+            user = User.objects.create(
+                username=username,
+                password=hashed_password,
+                email=email,
+                birth_date=birth_date,
+                usertype=usertype
+            )
+
+            return JsonResponse({'message': 'register success', 'username': user.username})
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'invalid JSON'}, status=400)
+
+    return JsonResponse({'error': 'method error'}, status=405)
